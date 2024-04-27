36da7135d24bb5239361d806001b1008669a9dc4
Your (user)name <>
2024-04-27 10:49:45 +0800

    add

--- deploy_with_ollama_qwen1.5_14b_q4.sh
+++ deploy_with_ollama_qwen1.5_14b_q4.sh
@@ -2,9 +2,17 @@
 # 安装vim
 sudo apt-get update
 sudo apt-get install -y vim
+sudo apt-get install netcat -y
+sudo apt-get install net-tools
+
 ## 安装ollama
 curl -fsSL https://ollama.com/install.sh | sh
 nohup ollama serve > ollama_server_log.txt 2>&1 &
+nohup ollama serve > ollama_server_log.txt 2>&1 &
+while ! netstat -tuln | grep ':11434' > /dev/null; do
+  sleep 1 # 每隔1秒检查一次
+done
+
 ollama run qwen:14b-chat-q4_K_S
 
 
